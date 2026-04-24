9bfdf23503d3427f2c7043b13fc4a5f978dbb396
Ava Lehner <avalehner@gmail.com>
2026-04-24 11:04:41 -0400

    added year menu to write page

--- client/src/pages/WritePage.tsx
+++ client/src/pages/WritePage.tsx
@@ -1,23 +1,37 @@
+import { useState } from "react"
 import EmotionMenu from "../components/EmotionMenu"
 import SeasonMenu from "../components/SeasonMenu"
+import YearMenu from "../components/YearMenu"
 
 const WritePage = () => {
+const [emotion, setEmotion] = useState<string>('')
+const [season, setSeason] = useState<string>('')
+const [year, setYear] = useState<number>(2026)
+
   return (
     <>
       <input 
         type="text"
         placeholder="enter title or artist"
       />
-      <input 
-        type="text"
-        placeholder="enter a month"
+      <p>listening to this song made me feel:</p>
+      <EmotionMenu 
+        emotion={emotion}
+        setEmotion={setEmotion}
       />
+      <p>this song reminds me of: </p>
+      <SeasonMenu 
+        season={season}
+        setSeason={setSeason}
+      />
+     <YearMenu 
+        year={year}
+        setYear={setYear}
+     />
       <input 
         type="text"
         placeholder="enter a memory"
       />
-      <EmotionMenu />
-      <SeasonMenu />
     </>
   )
 }
