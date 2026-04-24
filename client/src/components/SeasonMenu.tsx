0b0279ef9c6807a179cf554c1b5df595dce44f59
Ava Lehner <avalehner@gmail.com>
2026-04-24 11:04:28 -0400

    updated seasons meny with props

--- client/src/components/SeasonMenu.tsx
+++ client/src/components/SeasonMenu.tsx
@@ -1,9 +1,15 @@
 import styles from './css/SeasonMenu.module.css'
 
-const SeasonMenu = () => {
+interface SeasonMenuProps{
+  season: string
+  setSeason: (value:string) => void
+}
+
+
+const SeasonMenu = ({season, setSeason}: SeasonMenuProps) => {
   return (
     <div className={styles['emotion-menu-container']}>
-      <select className={styles['emotion-menu']}>
+      <select className={styles['emotion-menu']} value={season} onChange={() => setSeason}>
         <option value="spring">spring</option>
         <option value="summer">summer</option>
         <option value="fall">fall</option>
