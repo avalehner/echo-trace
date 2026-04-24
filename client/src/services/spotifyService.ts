76be3eb6ea6c3d848b15c0882308b6fb0ec6a798
Ava Lehner <avalehner@gmail.com>
2026-04-24 16:18:53 -0400

    updates to spotify service function

--- client/src/services/spotifyService.ts
+++ client/src/services/spotifyService.ts
@@ -1,13 +1,11 @@
 import type { SearchResult } from "../types"
 
-export const searchSongs = async (query: string):Promise<SearchResult> => {
-  const response = await fetch(`http//localhost:3000/api/search?q=${query}`, {
-    method: 'GET', 
-    headers: { 'Content-Type': 'application/json'},
-  })
+export const searchSongs = async (query: string):Promise<SearchResult[]> => { //dont need GET because fetch automatically defaults to GET
+  const response = await fetch(`http//localhost:3000/api/search?q=${query}`)
 
   if (!response.ok) throw new Error(`Server error: ${response.status}`)
-  const songData = await response.json()
+
+  const songData: SearchResult[] = await response.json()
   return songData
   
 }
