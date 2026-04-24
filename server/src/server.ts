3db3b7d3519d15545f6e5b28906c6ae19fc2624c
Ava Lehner <avalehner@gmail.com>
2026-04-24 15:50:43 -0400

    mounted routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express, { Express } from 'express'
 import cors from 'cors'
+import memoriesRouter from './routes/memories'
+import searchRouter from './routes/search'
 
 const app: Express = express()
 
@@ -8,7 +10,9 @@ const app: Express = express()
 app.use(express.json())
 app.use(cors({origin: 'http://localhost:5173'}))
 
-
+//routes
+app.use('api/memories', memoriesRouter)
+app.use('api/search', searchRouter)
 
 //server
 app.listen(3000, () => {
