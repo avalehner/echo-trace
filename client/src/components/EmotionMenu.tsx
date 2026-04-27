ae641b9c9b73c3f99ec989b3bba4a1862a4c1bfc
Ava Lehner <avalehner@gmail.com>
2026-04-27 14:40:04 -0400

    updated emotion selection

--- client/src/components/EmotionMenu.tsx
+++ client/src/components/EmotionMenu.tsx
@@ -11,15 +11,19 @@ const EmotionMenu = ({ emotion, setEmotion}: EmotionMenuProps) => {
       <select className={styles['emotion-menu']} value={emotion} onChange={(e)=> setEmotion(e.target.value)}>
         <option value="" disabled>select emotion</option>
         <option value="excited">excited</option>
+        <option value="wonder">wonder</option>
+        <option value="euphoria">euphoria</option>
+        <option value="sentimental">sentimental</option>
         <option value="nostalgic">nostalgic</option>
         <option value="heartbroken">heartbroken</option>
+        <option value="longing">longing</option>
         <option value="in-love">in love</option>
         <option value="melancholic">melancholic</option>
         <option value="anxious">anxious</option>
         <option value="peaceful">peaceful</option>
         <option value="lonely">lonely</option>
         <option value="bittersweet">bittersweet</option>
-        <option value="angry">angry</option>
+        <option value="anger">anger</option>
         <option value="depressed">depressed</option>
       </select>
     </div>
