fea4ef1d8186bbabd1f4ccaa39292d20ec5b00d9
Ava Lehner <avalehner@gmail.com>
2026-04-27 16:21:20 -0400

    updated get memories service function with filters for year, season, emotion

--- client/src/services/memoriesService.ts
+++ client/src/services/memoriesService.ts
@@ -1,10 +1,19 @@
 import type { MemoryTypes, NewMemoryTypes } from "../types"
 
-export const getAllMemories = async (): Promise<MemoryTypes[]> => {
-  const response = await fetch (`http://localhost:3000/api/memories`, {
-    method: 'GET', 
-    headers: { 'Content-Type': 'application/json'}, 
-  })
+interface MemoryFilters {
+  emotion?: string
+  season?: string
+  year?: number
+}
+
+export const getMemories = async (filters?: MemoryFilters): Promise<MemoryTypes[]> => {
+  const params = new URLSearchParams() //built in browaser API specifically designed for building query strings. handles all the formatting and encoding (adding ? = & etc) and encodes special characters 
+
+  if (filters?.emotion) params.append('emotion', filters.emotion)
+  if (filters?.season) params.append('season', filters.season)
+  if (filters?.year) params.append('year', String(filters.year)) //convert number to string bc url search params onlu accepts strings 
+
+  const response = await fetch (`http://localhost:3000/api/memories?${params.toString()}`) //when .toString() is called it returns a formatted query string 
 
   if(!response.ok) throw new Error(`Server error: ${response.status}`)
   
@@ -12,22 +21,12 @@ export const getAllMemories = async (): Promise<MemoryTypes[]> => {
   return memoriesData
 }
 
-export const getMemoryByEmotion = async (emotion: string): Promise<MemoryTypes[]> => {
-  const response = await fetch(`http://localhost:3000/api/memories/${emotion}`, {
-    method: 'GET', 
-    headers: { 'Content-Type': 'application/json' }, 
-  })
 
-  if(!response.ok) throw new Error(`Server error: ${response.status}`)
-    
-  const memoriesData = await response.json()
-  return memoriesData
-}
 
 export const createMemory = async (data: NewMemoryTypes): Promise<MemoryTypes> => {
   const response = await fetch(`http://localhost:3000/api/memories/`, {
     method: 'POST', 
-    headers: {'Content-type': 'application/json'}, 
+    headers: {'Content-Type': 'application/json'}, 
     body: JSON.stringify(data), 
   })
 
