713151899c692f43ce20650eac7195811110e248
Ava Lehner <avalehner@gmail.com>
2026-04-27 17:28:50 -0400

    added rout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router'
 import WritePage from './pages/WritePage'
+import ExplorePage from './pages/ExplorePage'
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<WritePage />} />
+      <Route path="/explore" element={<ExplorePage />} />
     </Routes>
   )
 }
