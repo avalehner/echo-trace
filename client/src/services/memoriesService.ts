0c402516aacd9f20fc07c8dcd9a721eb10c42253
Ava Lehner <avalehner@gmail.com>
2026-04-27 16:42:25 -0400

    added get memory by id, delete memory, and download wav service functions

--- client/src/services/memoriesService.ts
+++ client/src/services/memoriesService.ts
@@ -7,7 +7,7 @@ interface MemoryFilters {
 }
 
 export const getMemories = async (filters?: MemoryFilters): Promise<MemoryTypes[]> => {
-  const params = new URLSearchParams() //built in browaser API specifically designed for building query strings. handles all the formatting and encoding (adding ? = & etc) and encodes special characters 
+  const params = new URLSearchParams() //built in browser API specifically designed for building query strings. handles all the formatting and encoding (adding ? = & etc) and encodes special characters 
 
   if (filters?.emotion) params.append('emotion', filters.emotion)
   if (filters?.season) params.append('season', filters.season)
@@ -21,7 +21,14 @@ export const getMemories = async (filters?: MemoryFilters): Promise<MemoryTypes[
   return memoriesData
 }
 
+export const getMemoryById = async(id: string): Promise<MemoryTypes> => {
+  const response = await fetch(`http://localhost:3000/api/memories/${id}`)
 
+  if(!response.ok) throw new Error(`Server error: ${response.status}`)
+
+  const memoryData = await response.json()
+  return memoryData
+}
 
 export const createMemory = async (data: NewMemoryTypes): Promise<MemoryTypes> => {
   const response = await fetch(`http://localhost:3000/api/memories/`, {
@@ -34,4 +41,32 @@ export const createMemory = async (data: NewMemoryTypes): Promise<MemoryTypes> =
   
   const newMemoryData = await response.json()
   return newMemoryData
+}
+
+export const deleteMemory = async (id: string): Promise<MemoryTypes> => {
+  const response = await fetch(`http://localhost:3000/api/memories/${id}`, {
+    method: 'DELETE', 
+  })
+
+  if(!response.ok) throw new Error(`Server error: ${response.status}`)
+  
+  const deletedMemoryData = await response.json()
+  return deletedMemoryData
+}
+
+export const downloadWav = async (id:string): Promise<void> => {
+  const response = await fetch(`http://localhost:3000/api/memories/${id}/download`)
+
+  if (!response.ok) throw new Error(`Server error: ${response.status}`)
+
+  //triggering file download from fetch call 
+  const blob = await response.blob() //blob = binary large object - raw chunk of binary data (WAV file is in binary)
+  const url = URL.createObjectURL(blob) //creates a temporary URL That points to the blob in memory
+  const a = document.createElement('a') //creates a hidden anchor taf in memory 
+  a.href = url //points the anchor at the blob URL 
+  a.download = `memory-${id}.wav` //tells the browser that clicking this link should download the file 
+  document.body.appendChild(a) //append to DOM (some browsers require this for a click)
+  a.click() //clicks the anchor, triggers the download if the user clikced the download linke 
+  document.body.removeChild(a) //remove from DOM 
+  URL.revokeObjectURL(url) //cleans up the temporary blob URL from memory
 }
