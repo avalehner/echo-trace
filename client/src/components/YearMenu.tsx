e00f02390d624cf635ff98544ebcc76b1d51fd2e
Ava Lehner <avalehner@gmail.com>
2026-04-27 16:21:29 -0400

    updated types

--- client/src/components/YearMenu.tsx
+++ client/src/components/YearMenu.tsx
@@ -1,15 +1,15 @@
 import styles from './css/YearMenu.module.css'
 
 interface YearMenuProps {
-  year: number, 
-  setYear: (value: number) => void 
+  year: number | null, 
+  setYear: (value: number | null) => void 
 }
 
 const YearMenu = ({year, setYear}: YearMenuProps) => {
   return(
     <div className={styles['year-menu-container']} >
-      <select name='year' id='year' value={year} onChange={(e) => setYear(Number(e.target.value))}> {/*e.target.value is always a string*/}
-        <option value='0' disabled>select a year</option>
+      <select name='year' id='year' value={year ?? ''} onChange={(e) => setYear(Number(e.target.value))}> {/*e.target.value is always a string*/}
+        <option value='' disabled>select a year</option>
           {Array.from({ length: 77 }, (_, i) => 2026 - i).map(year => ( //Array.from() creates an array using the first param given, second param is a map function that transforms the array as it builds it 
             <option key={year} value={year}>{year}</option>
           ))}
