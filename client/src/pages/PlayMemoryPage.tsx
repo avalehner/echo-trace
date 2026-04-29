1fd633bdc51c5269194a9b6179d218497350c6b2
Ava Lehner <avalehner@gmail.com>
2026-04-29 14:16:52 -0400

    added logic to display decoded memory in real time

--- client/src/pages/PlayMemoryPage.tsx
+++ client/src/pages/PlayMemoryPage.tsx
@@ -13,6 +13,7 @@ const PlayMemoryPage = () => {
   const [isDecoding, setIsDecoding] = useState<boolean>(false)
   const [decodedMemory, setDecodedMemory] = useState<DecodedMemoryTypes | null>(null)
   const [displayedText, setDisplayedText] = useState<string>('')
+  const [fullDecodedText, setFullDecodedText] = useState<string>('')
 
   const { id } = useParams()
 
@@ -41,27 +42,36 @@ const PlayMemoryPage = () => {
   const handleDecodeMessage = async (memoryId: string) => {
     try {
       setIsDecoding(true)
-      await new Promise(resolve => setTimeout(resolve, 5000)) //makes async/await sleep for a duration
+      // await new Promise(resolve => setTimeout(resolve, 5000)) //makes async/await sleep for a duration
       const decoded = await decodeSong(memoryId)
       console.log('decoded', decoded)
       setDecodedMemory(decoded)
       const fullText = `emotion: ${decoded.emotion}, ` + `season: ${decoded.season} ` + `${decoded.year}, ` + `memory: ${decoded.memory_fragment}`
-      console.log('full text', fullText)
-      handleRevealText(fullText)
+      setFullDecodedText(fullText)
     } catch (error) {
       const message = error instanceof Error ? error.message : 'Unknown Error'
       console.error(message)
+    } finally {
+      setIsDecoding(false)
     }
   }
 
-  const handleRevealText = (memoryText: string) => {
-    let i = 0 
-    const interval = setInterval(() => {
-      setDisplayedText(memoryText.slice(0, i + 1))
-      i++
-      if (i >= memoryText.length) clearInterval(interval)
-    }, 150)
+  const handleTimeUpdate = (e: React.SyntheticEvent<HTMLAudioElement>) => { //type for event on audio element 
+    const currentTime = (e.target as HTMLAudioElement).currentTime //in seconds 
+    const samplesPlayed = currentTime * 44100
+    const bitsDecoded = samplesPlayed / 800 //added a bit every 800 samples 
+    const charactersVisible = bitsDecoded / 8 //each character is 8 bits
+    setDisplayedText(fullDecodedText.slice(0, Math.floor(charactersVisible)))
   }
+
+  // const handleRevealText = (memoryText: string) => {
+  //   let i = 0 
+  //   const interval = setInterval(() => { //setInterval() built in browser web API, returns an id 
+  //     setDisplayedText(memoryText.slice(0, i + 1))
+  //     i++
+  //     if (i >= memoryText.length) clearInterval(interval)
+  //   }, 150)
+  // }
 
   return (
     <div>
@@ -81,16 +91,21 @@ const PlayMemoryPage = () => {
         <p>artist:</p>
         <p>{memory.artist}</p>
       </div>
-      {!encodedSongUrl &&(<button onClick={()=> handleEncodedSong(memory.id)}>encode your memory</button>)}
+      {!encodedSongUrl &&(
+        <button onClick={()=> handleEncodedSong(memory.id)} >encode your memory</button>)}
       {isEncoding && <p>encoding your memory...</p>}
       {encodedSongUrl && (
         <div>
-          <audio controls src={encodedSongUrl} onPlay={() =>handleDecodeMessage(memory.id)}/>
+          {/* The ontimeupdate event occurs when the play time of a media changes. The ontimeupdate event occurs while the media is playing.The ontimeupdate event occurs when the user moves the play position. */}
+          <audio 
+            controls src={encodedSongUrl} 
+            onPlay={() =>!decodedMemory && handleDecodeMessage(memory.id)}
+            onTimeUpdate={handleTimeUpdate}/>
           <a href={encodedSongUrl} download={`memory-${id}.wav`}>download encoded song</a>
         </div>
       )}
       {encodedSongUrl && !decodedMemory && !isDecoding && <p>press play to decode your memory!</p>}
-      {isDecoding && !decodedMemory && <p>decoding your message...</p>}
+      {decodedMemory && <p>decoded message:</p>}
       {decodedMemory && <p>{displayedText}</p>}
     </div> 
   )
