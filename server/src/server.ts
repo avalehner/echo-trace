a637a3e5c5bbd94dcf43325712109b81f342c0ec
Ava Lehner <avalehner@gmail.com>
2026-05-01 12:18:56 -0400

    updated cors origin for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const app: Express = express()
 
 //middleware 
 app.use(express.json())
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors({origin: process.env.FRONTEND_URL || 'http://localhost:5173'}))
 
 //routes
 app.use('/api/memories', memoriesRouter)
