28b625ab57f208dad2af97b1d3c7f9de01e2c356
Ava Lehner <avalehner@gmail.com>
2026-09-16 19:21:06 -0400

    styling updates

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,18 +1,46 @@
+import { useState } from 'react';
 import styles from './css/NavBar.module.css';
-import { NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className={styles['nav-bar']}>
-      <ul>
+      <div className={styles['mobile-nav-header']}>
+        <Link className={styles['brand']} to="/" onClick={closeMenu}>
+          EchoTrace
+        </Link>
+        <button
+          className={`${styles['menu-toggle']} ${menuOpen ? styles['menu-toggle-open'] : ''}`}
+          type="button"
+          aria-label={menuOpen ? 'Close navigation menu' : 'Open navigation menu'}
+          aria-expanded={menuOpen}
+          aria-controls="primary-navigation"
+          onClick={() => setMenuOpen((open) => !open)}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
+      <ul id="primary-navigation" className={menuOpen ? styles['menu-open'] : undefined}>
         <li>
-          <NavLink to="/">ENTER MEMORY</NavLink>
+          <NavLink to="/" onClick={closeMenu}>
+            ENTER MEMORY
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/explore">VIEW ALL MEMORIES</NavLink>
+          <NavLink to="/explore" onClick={closeMenu}>
+            VIEW ALL MEMORIES
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">ABOUT</NavLink>
+          <NavLink to="/about" onClick={closeMenu}>
+            ABOUT
+          </NavLink>
         </li>
       </ul>
     </nav>
